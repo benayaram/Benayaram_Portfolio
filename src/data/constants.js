feec8f58567ecb9f1638c9775239a1c083b7afb9
benayaram rekha <96018533+benayaram@users.noreply.github.com>
2024-12-28 12:50:42 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,7 +13,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/benayaram",
   resume:
-    "https://drive.google.com/file/d/1iQSoD-OtXgcgWG9Xn563LiHVxE4XLyAE/view?usp=sharing",
+    "https://drive.google.com/file/d/1BWFgu0OSa-U7jaAqfweS2eTFwxSxDGIh/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rekha-benayaram/",
   youtube: "https://www.youtube.com/@benayaram",
 };
